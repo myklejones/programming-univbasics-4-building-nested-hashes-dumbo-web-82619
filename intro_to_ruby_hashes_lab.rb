7e11df14647b57bbf9eae6738e89fe8087647007
Mykle jones <mykle.j91@gmail.com>
2019-08-10 04:41:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,8 @@ def monopoly_with_third_tier
  anime =
 	{ railroads: {pieces:4,
 	rent_in_dollars:{
-	  names:{},key2:{},key3:{},key4:{}}
+	  names:{},key2:{},key3:{},key4:{}},
+	  names:{}
 	  
 	 },
 	}
