bb19b1f4baaf5a78940ec6d57c21a70cadcc59c0
Mykle jones <mykle.j91@gmail.com>
2019-08-10 04:45:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,9 @@ def monopoly_with_third_tier
 	{ railroads: {pieces:4,
 	rent_in_dollars:{
 	  one_piece_owned:25,
-	  two_piece_owned:50,
-	  three_piece_owned:75,
-	  four_piece_owned:100},
+	  two_pieces_owned:50,
+	  three_pieces_owned:75,
+	  four_pieces_owned:100},
 	  names:{key1:{},key2:{},key3:{},key4:{}}
 	  
 	 },
